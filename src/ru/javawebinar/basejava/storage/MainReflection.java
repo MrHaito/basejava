e8ac292ffcbc250588a33714ed037d15f4ecdd4d
o.klopskiy <abserber@gmail.com>
2022-06-11 17:07:08 +0300

    create reflection for toString

---
+++ src/ru/javawebinar/basejava/storage/MainReflection.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new");
+        System.out.println(r);
+
+        Method method = r.getClass().getMethod("toString");
+        System.out.println(method.invoke(r));
+    }
+}
