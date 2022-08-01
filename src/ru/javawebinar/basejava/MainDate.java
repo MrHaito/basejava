0fe37d9f4e7bbe7712b8fb57d61d47688eb42b6d
o.klopskiy <abserber@gmail.com>
2022-08-02 02:55:36 +0300

    lesson_10 tested LocalDate from String

--- src/ru/javawebinar/basejava/MainDate.java
+++ src/ru/javawebinar/basejava/MainDate.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
@@ -29,6 +30,12 @@ public class MainDate {
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yy/MM/dd");
         System.out.println(dtf.format(ldt));
+
+        LocalDate localDate = LocalDate.of(2013, Month.OCTOBER.getValue(), 1);
+        String strDate = "2013-10-01";
+        LocalDate localDate1 = LocalDate.parse(strDate);
+
+        System.out.println(localDate.equals(localDate1));
 
     }
 }
