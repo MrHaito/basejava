d7e9c34d8691322cf9ca32841a4fee38de730ca5
o.klopskiy <abserber@gmail.com>
2022-08-05 17:57:42 +0300

    lesson_11 deadlock

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private static volatile int counter;
-    private static final Object LOCK = new Object();
+    private static final Object LOCK1 = new Object();
+    private static final Object LOCK2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -50,12 +51,51 @@ public class MainConcurrency {
 
         LazySingleton.getInstance();
 
+
+        /* DEADLOCK START */
+
+        Thread t1 = new Thread(() -> {
+            synchronized (LOCK1) {
+                System.out.println("Thread 1: locked resource 1");
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK2) {
+                    System.out.println("Thread 1: locked resource 2");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            synchronized (LOCK2) {
+                System.out.println("Thread 2: locked resource 2");
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (LOCK1) {
+                    System.out.println("Thread 2: locked resource 1");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        /* DEADLOCK END */
     }
 
-    private void inc() {
+    private synchronized void inc() {
         counter++;
     }
 
-    /* DEADLOCK */
+
+
+
+
 
 }
