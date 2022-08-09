2732c3de103f0400bf1fe1ccfdca630d7a2b6242
o.klopskiy <abserber@gmail.com>
2022-08-09 19:52:14 +0300

    lesson_12 streams

--- src/ru/javawebinar/basejava/Streams.java
+++ src/ru/javawebinar/basejava/Streams.java
@@ -1,25 +1,34 @@
 package ru.javawebinar.basejava;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Streams {
     public static void main(String[] args) {
-        int[] intArray = new int[]{3,2,3,3,2,1};
-
+        int[] intArray = new int[]{3, 2, 3, 3, 2, 1};
         for (Integer integer : intArray) {
             System.out.print(integer + " ");
         }
         System.out.println("\n");
         System.out.println("minValue: " + minValue(intArray));
 
-
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 1; i <= 11; i++) {
+            integers.add(i);
+        }
+        System.out.print("oddOrEven: " + oddOrEven(integers));
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values)
-                .distinct()
-                .sorted()
-                .reduce(0, (a, b) -> 10 * a + b);
+        return Arrays.stream(values).distinct().sorted().reduce(0, (a, b) -> 10 * a + b);
     }
 
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream()
+                .filter((s) -> (s % 2 != 0 && integers.stream().reduce(0, Integer::sum) % 2 == 0)
+                        || (s % 2 == 0 && integers.stream().reduce(0, Integer::sum) % 2 != 0))
+                .collect(Collectors.toList());
+    }
 }
