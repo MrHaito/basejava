29a903a73df93cef4a1e57497dc35cb3b28cebe0
o.klopskiy <abserber@gmail.com>
2022-08-23 23:06:39 +0300

    add Override

--- src/ru/javawebinar/basejava/storage/strategy/ObjectStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/strategy/ObjectStreamSerializer.java
@@ -6,12 +6,14 @@ import ru.javawebinar.basejava.model.Resume;
 import java.io.*;
 
 public class ObjectStreamSerializer implements StorageStrategy {
+    @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(os)) {
             oos.writeObject(r);
         }
     }
 
+    @Override
     public Resume doRead(InputStream is) throws IOException {
         try (ObjectInputStream ois = new ObjectInputStream(is)) {
             return (Resume) ois.readObject();
