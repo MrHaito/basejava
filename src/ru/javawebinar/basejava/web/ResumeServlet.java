c4b6ccda677c70412ce83d8e31515c9dae423792
o.klopskiy <abserber@gmail.com>
2022-09-05 01:43:43 +0300

    lesson_15 refactor

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -4,7 +4,6 @@ import ru.javawebinar.basejava.Config;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
-import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +16,7 @@ public class ResumeServlet extends HttpServlet {
 
     private Storage storage;
 
-    public void init(Servlet config) throws ClassNotFoundException {
+    public void init()  {
         storage = Config.getInstance().getStorage();
     }
 
