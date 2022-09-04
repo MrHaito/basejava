a2559b18f9cfc6c5fe45a2fe8125a72a74461956
o.klopskiy <abserber@gmail.com>
2022-09-05 01:29:52 +0300

    lesson_15 refactor

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,8 +1,10 @@
 package ru.javawebinar.basejava.web;
 
+import ru.javawebinar.basejava.Config;
 import ru.javawebinar.basejava.model.Resume;
-import ru.javawebinar.basejava.storage.SQLStorage;
+import ru.javawebinar.basejava.storage.Storage;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +14,12 @@ import java.io.PrintWriter;
 import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
-    private final SQLStorage storage = new SQLStorage("jdbc:postgresql://localhost:5432/resumes", "postgres", "psqkm87nm2");
+
+    private Storage storage;
+
+    public void init(Servlet config) throws ClassNotFoundException {
+        storage = Config.getInstance().getStorage();
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
